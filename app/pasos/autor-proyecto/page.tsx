bd663c36a808b6cb78b084e6ab0fab0bde215a5b
Lenin Mejia <leninmejiacarlen@gmail.com>
2025-05-17 12:20:30 -0400

    correcion errores de componente

--- app/pasos/autor-proyecto/page.tsx
+++ app/pasos/autor-proyecto/page.tsx
@@ -64,7 +64,7 @@ export default function PasoAutorProyecto() {
         <div className="flex justify-end">
         {/* boton atras */}
         <button className="bg-[#003876] rounded-full h-8 w-8 text-white justify-center items-center  flex">
-          <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 6L5 12M5 12L11 18M5 12H19" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+          <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 6L5 12M5 12L11 18M5 12H19" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
         </button>
         </div>
 
